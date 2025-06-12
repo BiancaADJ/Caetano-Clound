34cf83ce25f35ec34bf3ce74cc20279f0489a9ce
Bianca <99222703+BiancaADJ@users.noreply.github.com>
2025-06-12 16:23:56 -0300

    Update 12-06-25

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,22 +9,24 @@ function login(action){
     fetch('http://localhost:8000/lib/index.php',{
       method: 'POST',
       mode: 'cors', // força requisição CORS
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ bd_user: bd_user, bd_pass: bd_pass, action: 'login' })
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({bd_user: bd_user, bd_pass: bd_pass, action: 'login'})
     })
     .then(response => response.json())
     .then(data => {
       console.log("Resposta do servidor:", data);
 
-      if (data && data.success) {
-        alert("Login bem-sucedido!");
+      if(data && data.success){
         console.log("Conexão bem-sucedida! Dados: " + JSON.stringify(data.data));
-      } else {
+        window.location.href = "http://localhost:8000/src/main.html";
+        localStorage.setItem('bd_user', bd_user);
+        localStorage.setItem('bd_pass', bd_pass);
+      }else{
         alert("Login e/ou senha inválido");
         console.error("Erro:", data.message);
       }
     })
-    .catch(error => {
+    .catch(error =>{
       console.error("Erro na requisição:", error);
       alert("Erro de conexão com o servidor.");
     });
@@ -36,3 +38,35 @@ function login(action){
     alert('Erro: parâmetro inválido em login(action)');
   }
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+  if(window.location.pathname.includes('/src/main.html')){ // Trocar página na versão final
+    var bd_user = localStorage.getItem('bd_user');
+    var bd_pass = localStorage.getItem('bd_pass');
+
+    fetch('http://localhost:8000/lib/index.php',{
+      method: 'POST',
+      mode: 'cors', // força requisição CORS
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({bd_user: bd_user, bd_pass: bd_pass, action: 'login'})
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Resposta do servidor:", data);
+
+      if(data && data.success){
+        console.log("Conexão bem-sucedida! Dados: " + JSON.stringify(data.data));
+        document.querySelector("#bem_vindo_nome").textContent = bd_user;
+      }else{
+        console.error("Erro:", data.message);
+        window.location.href = "http://localhost:8000/src/login.html";
+      }
+    })
+    .catch(error =>{
+      console.error("Erro na requisição:", error);
+      alert("Erro de conexão com o servidor.");
+      window.location.href = "http://localhost:8000/src/login.html";
+    });
+  }
+});
+
