9099acca6d3a79a3a394b968875406f340a1773e
Bianca <99222703+BiancaADJ@users.noreply.github.com>
2025-09-25 14:51:24 -0300

    Update 25.09.25

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from '../assets/img/react.svg'
+import viteLogo from '../assets/img/vite.svg'
+import '../assets/css/App.css'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
